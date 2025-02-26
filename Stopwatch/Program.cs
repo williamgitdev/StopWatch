de3b8199c77705b2c174d55de9b9dbf739b7e070
William Alves da Silva <williamgitdev@hotmail.com>
2025-02-26 01:22:37 -0300

    Criacao do Menu

--- Stopwatch/Program.cs
+++ Stopwatch/Program.cs
@@ -6,12 +6,19 @@ namespace StopWatch
     {
         static void Main(string[] args)
         {
-            Start();
+            Menu();
         }
 
-        static void Start()
+        static void Menu()
         {
-            int time = 10;
+            Console.Clear();
+            Console.WriteLine("S - Seconds");
+            Console.WriteLine("M - Minutes");
+            Console.WriteLine("How much time do you want to count?");
+        }
+
+        static void Start(int time)
+        {
             int currentTime = 0;
 
             while (currentTime != time)
@@ -21,6 +28,9 @@ namespace StopWatch
                 Console.WriteLine(currentTime);
                 Thread.Sleep(1000);
             }
+            Console.Clear();
+            Console.WriteLine("Time is up!");
+            Thread.Sleep(2500);
         }
     }
 }
