eb5f95e60f1985c44eea07b193a02aa2728b7b63
William Alves da Silva <williamgitdev@hotmail.com>
2025-02-26 01:00:34 -0300

    Estrutura do cronometro

--- Stopwatch/Program.cs
+++ Stopwatch/Program.cs
@@ -8,5 +8,16 @@ namespace StopWatch
         {
             Console.WriteLine("Hello World!");
         }
+
+        static void Start()
+        {
+            int time = 10;
+            int currentTime = 0;
+
+            while (currentTime != time)
+            {
+                currentTime++;
+            }
+        }
     }
 }
